f77b5382af23eebd29989a333e15398141a747f1
Karthikeya-t <65700550+Karthikeya-t@users.noreply.github.com>
2022-07-17 16:49:38 +0530

    first commit 2t

---
+++ js/config.js
@@ -0,0 +1,18 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 1.change this address to your own address
+// 2.set how much fee you will recieve,for example:0.01 
+// 3.client total fee= contract small fee + your setting fee
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var FeeReciever = "0x2C448C7bcF08030Cdf96ea4F41E7307d9d6Ee860";
+
+
+var BinanceFee=0.01;
+var PolygonFee=0.001;
+var FantomFee=0.12;
+var RopstenFee=0.00001;
+var HuobiFee=0.01;
+var AvalancheFee=0.1;
+var EthereumFee=0.001;
+var CronosFee=3;
+var RinkebyFee=0.001;
